163898e39f0b6478096acc03b579055ea8dec516
Lizbeth Garnica <garnic09@hotmail.com>
2024-01-12 18:48:33 -0600

    LGN 12/01/24 Tuplas

---
+++ env/tema1-Variables.py
@@ -0,0 +1,24 @@
+#Nombre de variables
+
+'''
+Comentarios
+Multilinea
+'''
+
+numero1="23"
+numero2="10"
+nombre="Domingo"
+correo="domingo@gmail.com"
+
+print(numero1+numero2)
+
+#Convercion forzosa entre tipos de datos
+print("La suma es: ")
+print(int(numero1)+int(numero2))
+
+sueldo = "938.284"
+valorDecimal=float(sueldo)
+print(valorDecimal)
+
+edad = 283
+print(len(str(edad)))
